0c36888f109de14997c584cd3e06a9dc74b41bf0
almirpask <almirpask@hotmail.com>
2019-02-23 23:19:58 -0300

    finish app

--- phrases of the day/ViewController.swift
+++ phrases of the day/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pharase: UILabel!
+    
+    @IBAction func newPharase(_ sender: UIButton) {
+        let pharases =  ["Bom dia Caralho!", "Travesti sem Penis é igual Anjo sem Asa!", "Chamo de mae não respondeu, o pau comeu", "Todo Otaku é fedido!", "Eh mole kkkk"]
+        pharase.text = String(pharases[Int.random(in: 1 ..< pharases.count - 1)])
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

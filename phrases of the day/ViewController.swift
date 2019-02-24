2050a54d0fdcdcce4e091c9bbf225e889881a2f2
almirpask <almirpask@hotmail.com>
2019-02-23 22:51:39 -0300

    Initial Commit

---
+++ phrases of the day/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  phrases of the day
+//
+//  Created by Almir Santos on 23/02/19.
+//  Copyright © 2019 Almir Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
